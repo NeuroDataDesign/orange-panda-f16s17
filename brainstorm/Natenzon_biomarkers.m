20c158c96a703ee4ddd0c206baf23fad02630643
mnatenz1 <mnatenz1@jhu.edu>
2016-06-29 17:36:41 -0400

    Create Natenzon_biomarkers.m

---
+++ brainstorm/Natenzon_biomarkers.m
@@ -0,0 +1,23 @@
+#Sweat Sensor
+
+##Pros
+  -Real time updates
+  -Non-invasive
+  -Measure's multiple biomarkers at once
+
+##Cons
+  -All sensors must be custom fabricated (from basic chemicals)
+  -Requires sweat
+  -Not as accurate as blood tests
+  -Sweat can be contaminated by  microbes
+  -Must be calibated to temperature
+  -Electrochemical sensors are very sensitive to temp / skin temp
+
+
+#Biomarkers
+  -sweat composition:
+    -glucose / lactate / sodium / potassium / body temperature
+  -electrical signals generated upon contact with sweat
+
+##Applications
+  -Athletics
