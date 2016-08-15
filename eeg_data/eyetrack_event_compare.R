0452f843c6bf5cdab86df5ac50c1290c6b0cd55d
Nitin Kumar <nkumar14@jhu.edu>
2016-08-15 15:49:53 -0400

    Tried for a new plot

--- eeg_data/eyetrack_event_compare.R
+++ eeg_data/eyetrack_event_compare.R
@@ -9,5 +9,10 @@ eventcompare <- function(eventdata) {
 
 bytime <- function(eventdata) {
     library(ggplot2);
-    
+    # First, get the R user event timestamps
+    usertable <- eventdata$userevent[, c("Start", "Description")];
+    lowerbound <- usertable$Start[1];
+    upperbound <- usertable$Start[2];
+    out <- rbind(eventdata$blink[, c("EventType", "Trial")], eventdata$saccade[, c("EventType", "Trial")],
+                 eventdata$fixation[, c("EventType", "Trial")]);
 }
