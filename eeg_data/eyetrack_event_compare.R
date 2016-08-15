ebc8b72ffa536f3812d49bee9c74e67397784385
Nitin Kumar <nkumar14@jhu.edu>
2016-08-15 15:34:51 -0400

    Added labels and titles

--- eeg_data/eyetrack_event_compare.R
+++ eeg_data/eyetrack_event_compare.R
@@ -4,5 +4,10 @@ eventcompare <- function(eventdata) {
     library(ggplot2);
     out <- rbind(eventdata$userevent[, c("EventType", "Trial")], eventdata$blink[, c("EventType", "Trial")],
     eventdata$saccade[, c("EventType", "Trial")], eventdata$fixation[, c("EventType", "Trial")]);
-    qplot(out$EventType);
+    qplot(out$EventType, xlab = "Event Type", ylab = "Occurrences", main = "Event Occurrences");
+}
+
+bytime <- function(eventdata) {
+    library(ggplot2);
+    
 }
