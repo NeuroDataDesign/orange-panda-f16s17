7fdf7cb0210bf212c9a9baa0e022b3cc120706e4
Nitin Kumar <nkumar14@jhu.edu>
2016-08-20 14:09:50 -0400

    Added eeg reading file

---
+++ eeg_data/eeg_read.R
@@ -0,0 +1,28 @@
+# convert .mat to .rdsc
+convertmetadata <- function(inputpath) {
+    # start by making sure R.matlab is available in environment
+    # use suppress warnings because don't want warning if R.matlab
+    # isn't present
+    if(suppressWarnings(!require("R.matlab"))) {
+        install.packages("R.matlab");
+        library("R.matlab");
+    }
+    
+    # next, readMat the file in
+    return(readMat(file(inputpath), maxLength=NULL, fixNames=TRUE, drop=c("singletonLists"),
+            sparseMatrixClass=c("Matrix", "SparseM", "matrix"), verbose=FALSE));
+    
+}
+
+converteeg <- function(inputpath) {
+    # first install required package to handle
+    # hdf5 format used in the files provided
+    if(suppressWarnings(!require("h5"))) {
+        install.packages("h5");
+        library(h5);
+    }
+    
+    summary(h5file(inputpath, mode="r")[, "resultf <- h5file("ex-matlab.mat", "r")
+]);
+    
+}
