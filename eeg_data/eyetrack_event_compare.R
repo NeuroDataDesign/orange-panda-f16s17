c2749b8e9398c8042f454782a6f9ec74bba8f4b3
tsunwong123 <twong37@jhu.edu>
2016-08-19 20:29:22 -0400

    Two plots on saccade duration and distance

--- eeg_data/eyetrack_event_compare.R
+++ eeg_data/eyetrack_event_compare.R
@@ -16,3 +16,21 @@ bytime <- function(eventdata) {
     out <- rbind(eventdata$blink[, c("EventType", "Trial")], eventdata$saccade[, c("EventType", "Trial")],
                  eventdata$fixation[, c("EventType", "Trial")]);
 }
+
+
+#Shows the duration of saccades via the chronological trial numbers
+saccadeduration <- function(eventdata) {
+  library(ggplot2)
+  out1 <- rbind(eventdata$saccade[, c("EventType", "Number", "Duration")]);
+  g <- ggplot(out1, aes(Number, Duration))
+  g + geom_point() + facet_grid(.~ EventType)
+}
+
+#Shows how far the subject's eyes have moved during a saccade
+saccadedistance <- function(eventdata) {
+  library(ggplot2)
+  out1 <- rbind(eventdata$saccade[, c("EventType","Number","StartLoc.X", "StartLoc.Y", "EndLoc.X", "EndLoc.Y")]);
+  out1$distance <- sqrt((out1$EndLoc.X-out1$StartLoc.X)^2+(out1$EndLoc.Y - out1$StartLoc.Y)^2)
+  g <- ggplot(out1, aes(Number, distance))
+  g + geom_point() + facet_grid(.~ EventType) + labs(x = "Trial Number", y = "Distance Traveled by Eye")
+}
