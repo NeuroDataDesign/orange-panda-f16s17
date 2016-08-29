64960010a11859b656d7eda6197032570fa00d5a
Nitin Kumar <nkumar14@jhu.edu>
2016-08-29 15:19:19 -0400

    Added generalized readeegdata (finally) and cleaned some unneccessary code from eyetrack_event_read)

--- eeg_data/read-data/read_data.R
+++
@@ -1,9 +0,0 @@
-# This file has the wrapper functions that we will interface with.
-# Each will use the amazon functions and the read functions to return
-# R usable data based on what's available. The underlying behavior will be
-# hidden from any user.
-
-readeeg <- function(patientnum = ) {
-    # First, load dependencies for Amazon access
-    
-}
