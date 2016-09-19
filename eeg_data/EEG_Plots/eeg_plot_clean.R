946f810b0d10141a96a031a818a32e7414ca756b
Nitin Kumar <nkumar14@jhu.edu>
2016-09-19 15:41:21 -0400

    Added significant changes to Fourier code so that now plots accurate transform data

--- eeg_data/EEG_Plots/eeg_plot_clean.R
+++ eeg_data/EEG_Plots/eeg_plot_clean.R
@@ -1,54 +1,81 @@
-eegsubset <- function(eegdata) {
-  eegsubsetting1min <- eeg[1:30000,];
-  eegsubsetting1min15 <- eegsubsetting1min[seq(1, nrow(eegsubsetting1min), 15), ];
-  labels <- c('Time','E1','E2','E3','E4','E5','E6','E7','E8','E9','E10','E11','E12','E13','E14','E15','E16','E18','E19','E20','E21','E22','E23','E24','E25','E26','E27','E28','E29','E30','E31','E32','E33','E34','E35','E36','E37','E38','E39','E40','E41','E42','E43','E44','E45','E46','E47','E50','E51','E52','E53','E54','E55','E57','E58','E59','E60','E61','E62','E64','E65','E66','E67','E69','E70','E71','E72','E74','E75','E76','E77','E78','E79','E80','E82','E83','E84','E85','E86','E87','E89','E90','E91','E92','E93','E95','E96','E97','E98','E100','E101','E102','E103','E104','E105','E106','E108','E109','E110','E111','E112','E114','E115','E116','E117','E118','E120','E121','E122','E123','E124','Cz')
-  colnames(eegsubsetting1min15) <- labels
+# fftshift code adapted and altered from 
+# http://stackoverflow.com/questions/38230794/how-to-write-fftshift-and-ifftshift-in-r
+fftshift <- function(input_matrix, dim = -1) {
+    cols <- length(input_matrix)
+    cols_half <- ceiling(cols/2)
+    return(c(input_matrix[(cols_half+1):cols], input_matrix[1:cols_half]))
 }
 
-eegplot <- function(eegdata) {
-  library(ggplot2)
-  ggplot(eegsubsetting1min15, aes(Time, Cz)) + geom_line() + ggtitle("Voltage of the Cz Electrode Every 30 ms") +
+# fft wrapper
+# run the fft, fftshift
+convert.fft <- function(eegdata) {
+    # run fft
+    fftdata <- fft(eegdata);
+    # fftshift
+    fftdata <- fftshift(fftdata);
+    return(fftdata);
+}
+
+# Subset the data
+
+eegsubsetter <- function(data, timeslice = c(1, nrow(data)), downsample = 1) {
+    # incorporate timeslice
+    subset <- data[timeslice[1]:timeslice[2],,];
+    # then downsample
+    return(subset[seq(1, nrow(subset), downsample), ])
+}
+
+# Plot functions
+
+# Time domain plot of the eegdata
+timeplot <- function(eegdata, electrodes) {
+  require(ggplot2)
+  ggplot(eegdata, aes(Time, Cz)) +
+    geom_line() +
+    ggtitle("Voltage of the Cz Electrode Every 30 ms") +
     labs(x="Time (ms)",y="Voltage of Cz (??V)");
 }
 
-powerspecplot <- function(eegdata) {
-  fftCz <- fft(eegdata$Cz);
-  print(length(fftCz)/2);
-  #fftCz <- fftCz[1:(nrow(fftCz)/2)];
-  spectrum(fftCz, main = "CZ Electrode Frequency Graph", xlab = "frequency (kHz)", ylab = "Power (W)", xlim = c(0, .25));
+# Amplitude vs Frequency Plot of given data
+ampplot <- function(fftdata, electrodes, dur, upfreq = 500) {
+    # dependency
+    require(ggplot2);
+    fftdata <- apply(fftdata, FUN = Mod, MARGIN = 2);
+    amp <- as.data.frame(cbind(Frequency = (1:nrow(fftdata))/dur, fftdata));
+    amp <- melt(amp, id.vars = "Frequency", variable_name = "Electrode");
+    ggplot(amp, aes(y = value, x = Frequency, color = Electrode)) +
+        geom_line() +
+        ggtitle(paste(electrodes)) +
+        xlim(0,upfreq) +
+        ylim(0,1000) +
+        labs(x="Frequency (Hz)",y="Amplitude (dB)");
 }
 
-#convert.fft is a courtesy of http://www.di.fc.ul.pt/~jpn/r/fourier/fourier.html
-convert.fft <- function(cs, sample.rate=500) {
-  cs <- cs / length(cs) # normalize
-
-  distance.center <- function(c)signif( Mod(c),        4)
-  angle           <- function(c)signif( 180*Arg(c)/pi, 3)
-  
-  df <- data.frame(cycle    = 0:(length(cs)-1),
-                   freq     = 0:(length(cs)-1) * sample.rate / length(cs),
-                   strength = sapply(cs, distance.center),
-                   delay    = sapply(cs, angle))
-  return(df);
+# Phase vs Frequency Plot of given data
+phasespecplot <- function(fftdata, electrodes) {
+    # dependency
+    require(ggplot2);
+    phase <- as.data.frame(cbind((1:length(fftdata))/dur, Arg(fftdata)));
+    colnames(phase) <- c("Frequency", "Amount");
+    ggplot(phase, aes(y = Amount, x = Frequency)) +
+        geom_line() +
+        ggtitle(paste(electrodes, "Amplitude vs Frequency")) +
+        xlim(0,100) +
+        labs(x="Frequency (Hz)",y="Amplitude (dB)");
 }
 
-ampspecplot <- function(fftdata) {
-  AmpCz <- Mod(fftdata)
-  spectrum(AmpCz, xlab = "frequency (kHz)", ylab = "Amplitude (dB)");
-}
+# Higher level plotter functions
 
-phasespecplot <- function(fftdata) {
-  phaseCz <- Arg(fftdata)
-  spectrum(phaseCz, xlab = "frequency (kHz)", ylab = "Phase");
-}
-
-plot.frequency.spectrum <- function(X.k, xlimits=c(0,length(X.k)/2)) {
-    plot.data  <- cbind(0:(length(X.k)-1), Mod(X.k))
-    
-    # TODO: why this scaling is necessary?
-    plot.data[2:length(X.k),2] <- 2*plot.data[2:length(X.k),2] 
-    
-    plot(plot.data, t="h", lwd=2, main="", 
-         xlab="Frequency (Hz)", ylab="Strength", 
-         xlim=xlimits, ylim=c(0,max(Mod(plot.data[,2]))))
+# Look at different electrodes in graph
+fourierwrapper <- function(eegdata, electrodes = "Cz", timeslice = c(1, 30000),
+                           samplerate = 500, upperfreq = 250) {
+    # subset
+    eegsubset <- eegsubsetter(eegdata, timeslice = timeslice);
+    # make ffts of multiple electrodes
+    eegsubset <- as.data.frame(eegsubset[, electrodes]);
+    colnames(eegsubset) <- electrodes;
+    fftsubset <- apply(eegsubset, FUN = convert.fft, MARGIN = 2);
+    # plot the electrodes
+    ampplot(fftsubset, electrodes = electrodes, 
+            dur = timeslice[2]/samplerate, upfreq = upperfreq)
 }
