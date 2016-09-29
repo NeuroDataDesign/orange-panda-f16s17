4dfa2fff65e982f764f842d35e48bd463e2acca0
Ryan Marren <rmarren1@jhu.edu>
2016-09-29 19:44:03 -0400

    updated notebook and plotting library

--- pipeline/utils/plots.py
+++ pipeline/utils/plots.py
@@ -6,15 +6,16 @@ def plot_timeseries(data, time = None, selector = "all", start = 0, end = None, 
     plt.figure()
     numchan = int(data.shape[1])
     numtimesteps = int(data.shape[0])
-    if end == None: end = numtimesteps
-    if time == None: time = range(end)
+    if end is None: end = numtimesteps
+    if time is None: time = range(end)
     if selector == "all":
         selector = range(numchan)
     elif selector == "random":
-        selector = random.sample(numchan, randno)
+        selector = random.sample(range(numchan), randno)
     times = time[start:end:skip]
     for channel in selector:
         d = data[:, channel][start:end:skip]
+        assert len(times) == len(d)
         plt.plot(times, d)
     plt.title(title)
     plt.xlabel(xlab)
