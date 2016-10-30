4b2dd4449da01eeab8a5eeec69492b5a65cfe7ac
Ryan Marren <rmarren1@jhu.edu>
2016-10-30 17:45:47 -0400

    updated meda.py with more functionality

--- pipeline/src/utils/meda.py
+++ pipeline/src/utils/meda.py
@@ -1,3 +1,5 @@
+from plotly.tools import FigureFactory as FF
+import scipy.interpolate as interpolate
 import colorlover as cl
 from scipy.signal import spectrogram
 from pandas_datareader import data
@@ -32,6 +34,7 @@ def type_plot(df):
             legend=False,
             yTitle='Density',
             secondary_y = ['Freq'])
+    fig.layout.update(dict(yaxis2=dict(title="Frequency", side="right")))
     return fig
 
 def bad_values(df, choice='NaN', axis='Column'): 
@@ -57,6 +60,8 @@ def bad_values(df, choice='NaN', axis='Column'):
             xTitle= axis,
             shared_xaxes=True,
             fill=True)
+    fig.layout.update(dict(yaxis2=dict(title=choice + " Frequency",
+                    side="right")))
     return fig
 
 def var_dist(df, axis='Column'): 
@@ -64,18 +69,25 @@ def var_dist(df, axis='Column'):
         df = df.transpose()
     df = df.replace([np.inf, -np.inf], np.nan).dropna(how="all")
     var = df.var()
-    mean = var.mean()
-    mcvar = var - mean
-    mcvar = mcvar.rename(
+    minimum = var.min()
+    maximum = var.max()
+    logscale = ""
+    if float(minimum) / maximum < .1:
+        logscale = "logscale"
+        var = df.apply(np.log, axis=1).var()
+        minimum = var.min()
+        maximum = var.max()
+    pad = maximum - minimum / 10.0
+    var = var.rename(
             columns = {0 : 'Variances'})
-    fig = mcvar.iplot(kind='bar',
+    fig = var.iplot(kind='bar',
            legend=False,
-           yTitle= 'Variance',
            asFigure=True,
-           title='Variance for each ' + axis + ' (mean centered, mean = ' + \
-                   str(np.round(mean, 2)) + ',  excluding all NaN and Inf)',
+           title='Variance for each ' +axis+ ' (excluding all NaN and Inf)',
            xTitle= axis,
            fill=True)
+    fig.layout.update(dict(yaxis=dict(title = 'Variance ' + logscale,
+        range=[minimum - pad, maximum + pad])))
     return fig
 
 def my_spectrogram(df, ind, sfreq):
@@ -84,9 +96,52 @@ def my_spectrogram(df, ind, sfreq):
     col = df.as_matrix()[:, ind]
     f, t, Sxx = spectrogram(col.T, sfreq)
     sp = pd.DataFrame(data=np.log(Sxx.T), index = t, columns = f)
-    return sp.iplot(kind='heatmap', colorscale='spectral', asFigure=True,
-            title="Spectrogram for " + cols[ind] + ' (log magnitude)',
+    fig = sp.iplot(kind='heatmap', colorscale='spectral', asFigure=True,
+            title="Spectrogram for " + cols[ind],
             xTitle='Time', yTitle = 'Frequency', zTitle="log magnitude")
+    fig.data.update(dict(colorbar=dict(title="Magnitude logscale", titleside="right")))
+    return fig
+
+def anomaly(df):
+    df = df.replace([np.inf, -np.inf], np.nan).fillna(0)
+    mcdf = df - df.mean()
+    U, S, Vt = np.linalg.svd(mcdf.as_matrix(), full_matrices = False)
+    df = (df).dot(U[0, :])
+    df.columns=['Scatter Points']
+    spline = df.rolling(window=df.shape[0]/50, center=True, min_periods = 0).mean()
+    spline.columns=['Smoothed Values']
+    resid = (df - spline).apply(np.abs)
+    sd = np.sqrt(resid.var())
+    hcb = spline + 1 * sd
+    lcb = spline - 1 * sd
+    fig = df.iplot(title="Anomaly Detection Plot (outside 1 SD bands from rolling mean)",
+            xTitle='Time', yTitle='Value', 
+            kind='line', mode='markers', asFigure=True, legend=True)
+    fig.data.update(dict(marker=dict(size = 6)))
+    fig.data.extend(spline.iplot(kind='line', asFigure=True, color='blue', legend=True).data)
+    fig.data.extend(hcb.iplot(kind='line', asFigure=True, color='red', legend=True).data)
+    fig.data.extend(lcb.iplot(kind='line', asFigure=True, color='red', legend=True).data)
+    return fig
+
+
+def cv(df):
+    df = df.replace([np.inf, -np.inf], np.nan).fillna(0)
+    mcdf = df - df.mean()
+    U, S, Vt = np.linalg.svd(mcdf.as_matrix(), full_matrices = False)
+    S = S**2
+    total = np.sum(S)
+    S = np.cumsum(S)
+    S = S / total
+    index = (np.arange(S.shape[0]) + 1).tolist()
+    scree = pd.DataFrame(data = S.tolist(), index = range(1, len(df.columns) + 1), columns=["eigenvalue"])
+    fig = scree.iplot(kind='line', fill = True,
+            title="Cumulative Variance Explained",
+            asFigure=True)
+    ytitle = "Cumulative Proportion of Total Variance Explained"
+    xtitle = "Principal Component"
+    fig.layout.update(dict(yaxis=dict(tick0=0, title = ytitle),
+        xaxis=dict(tickvals=index, title=xtitle)))
+    return fig 
 
 
 def sparklines(df):
@@ -104,86 +159,41 @@ def sparklines(df):
 
 def heatmap(df):
     cp = ''
+    minimum = df.min().min()
+    maximum = df.max().max()
+    mean = df.mean().mean()
+    logscale = ""
+    if float(mean) / maximum < .1 or float(minimum) / mean < .1:
+        logscale = "logscale"
+        var = df.apply(np.log, axis=1).var()
+        minimum = var.min()
+        maximum = var.max()
     if len(df.index) > 1000:
         cp = "(time compressed from " + str(len(df.index)) + " to 1000)"
         index = np.floor(np.linspace(0, len(df.index) - 1, 1000))
         df = df.ix[index.astype(int)]
-    return df.iplot(kind='heatmap', colorscale='spectral', asFigure=True,
+    df = df.apply(np.log, axis=1)
+    fig = df.iplot(kind='heatmap', colorscale='spectral', asFigure=True,
             title="Heatmap over time " + cp, xTitle='Row Index',
             yTitle = 'Column Index')
-
-def correlation(df):
-    correl = df.corr(method='pearson').as_matrix()
-    data = [
-        go.Heatmap(
-            z = correl.T,
-            colorbar=go.ColorBar(title='Correlation coefficient (rho)'),
-            zmin = -1,
-            zmax = 1
-        )
-    ]
-    layout = go.Layout(
-        title='Pearson Correlation Matrix',
-        xaxis=dict(
-            title='Columns',
-            titlefont=dict(
-                family='Courier New, monospace',
-                size=18,
-                color='#7f7f7f'
-            )
-        ),
-        yaxis=dict(
-            title='Columns',
-            autorange='reversed',
-            titlefont=dict(
-                family='Courier New, monospace',
-                size=18,
-                color='#7f7f7f'
-            )
-        )
-    )
-    fig = go.Figure(data=data, layout=layout)
-    #fig['layout'].update(height = 800, width = 800, autosize=False)
+    fig.data.update(dict(colorbar=dict(title="Magnitude " + logscale, titleside="right")))
     return fig
 
-def special_dist(M):
-    num_cols = M.shape[1]
-    num_rows = M.shape[0]
-    npr = map(lambda x: np.sum(np.isnan(M[x, :])),
-            xrange(num_rows))
-    npc = map(lambda x: np.sum(np.isnan(M[:, x])),
-            xrange(num_cols))
-    ipr = map(lambda x: np.sum(np.isinf(M[x, :])),
-            xrange(num_rows))
-    ipc = map(lambda x: np.sum(np.isinf(M[:, x])),
-            xrange(num_cols))
-    mr = map(lambda x: np.nanmean(M[x, :]),
-            xrange(num_rows))
-    mc = map(lambda x: np.nanmean(M[:, x]),
-            xrange(num_cols))
-    vr = map(lambda x: np.nanvar(M[x, :]),
-            xrange(num_rows))
-    vc = map(lambda x: np.nanvar(M[:, x]),
-            xrange(num_cols))
-    return npr, npc, ipr, ipc, mr, mc, vr, vc
-
-
-
-def plotly_hist(data, x_ticks):
-    data = [
-        go.Histogram(
-            x = data
-        )
-    ]
-    layout = go.Layout(
-            xaxis = go.XAxis(dict(ticktext=x_ticks,
-                tickvals=range(len(labels))),
-                showticklabels=True,
-                range=[0, len(labels)],
-                mirror=True),
-        )
-    figure = go.Figure(data=data, layout=layout)
-    return plotly_hack(figure)
+def correlation(df):
+    correl = np.fliplr(df.corr(method='pearson').as_matrix())
+    x = df.columns.format()
+    y = x[::-1]
+    z_text = np.around(correl, 2)
+    colorscale = [[-1, '#0000FF'], [1, '#FF0000']]  # custom colorscale
+    print x
+    print y
+    print z_text
+    fig = FF.create_annotated_heatmap(correl, x=x, y=y, annotation_text=z_text)
+    fig.layout.update(dict(title="Pearson Correlation Matrix", height = 800, width = 800, autosize = False))
+    fig.data.update( 
+            dict(colorscale = colorscale, showscale = True,
+                colorbar=dict(title="correlation coefficient")))
+    return fig
 
 def wrap_html(html):
     out = ''
@@ -220,29 +230,32 @@ def get_messy_data(df):
 
 if __name__ == "__main__":
     cf.go_offline()
-    iris = data.DataReader("yhoo", "yahoo", datetime.datetime(2007, 1, 1), 
-                datetime.datetime(2012,1,1))
-   # iris = pdr.get_data_yahoo('AAPL')
-    iris = iris.drop('Volume', 1)
-    #iris = pd.read_csv('iris.csv')
-    iris = pd.DataFrame(get_messy_data(iris), index = iris.index, columns = iris.columns)
-    iris = iris.apply(lambda x: pd.to_numeric(x, errors='ignore'))
+    start = datetime.datetime(2010, 10, 10)
+    end = datetime.datetime(2016,10,10)
+    companies = ["goog", "aapl", "yhoo", "msft", "amzn"]
+    cnames = ["Google",  "Apple", "Yahoo", "Microsoft", "Amazon"]
+    df = pd.concat([data.DataReader(c, "yahoo", start, end).loc[:, "Close"] for c in companies], axis=1)
+    df.columns = cnames
+    df = pd.DataFrame(get_messy_data(df), index = df.index, columns = df.columns)
+    df = df.apply(lambda x: pd.to_numeric(x, errors='ignore'))
     html = ''
     #html += plotly_hack(type_plot(iris))
-    html += plotly_hack(type_plot(iris))
-    html += plotly_hack(bad_values(iris, 'NaN', 'Column')) 
-    html += plotly_hack(bad_values(iris, 'Inf', 'Column')) 
-    html += plotly_hack(bad_values(iris, 'NaN', 'Row')) 
-    html += plotly_hack(bad_values(iris, 'Inf', 'Row')) 
-    html += plotly_hack(var_dist(iris, 'Column')) 
+    html += plotly_hack(type_plot(df))
+    html += plotly_hack(bad_values(df, 'NaN', 'Column')) 
+    html += plotly_hack(bad_values(df, 'Inf', 'Column')) 
+    html += plotly_hack(bad_values(df, 'NaN', 'Row')) 
+    html += plotly_hack(bad_values(df, 'Inf', 'Row')) 
+    html += plotly_hack(var_dist(df, 'Column')) 
     #html += var_dist(iris, 'Row') 
-    html += plotly_hack(heatmap(iris)) 
-    html += plotly_hack(sparklines(iris)) 
-    for i in range(len(iris.columns)):
-        html += plotly_hack(my_spectrogram(iris, i, 1))
-    neg_iris = iris.copy()
-    neg_iris.ix[:, :] = np.random.random(iris.shape)
-    html += plotly_hack(correlation(neg_iris))
+    html += plotly_hack(heatmap(df)) 
+    html += plotly_hack(sparklines(df)) 
+    d = len(df.columns)
+    sp_compress = np.floor(np.linspace(0, d-1, min(d, 10)))
+    for i in sp_compress:
+        html += plotly_hack(my_spectrogram(df, i, 1))
+    html += plotly_hack(correlation(df))
+    html += plotly_hack(cv(df))
+    html += plotly_hack(anomaly(df))
     html = wrap_html(html)
     with open('meda_example.html', 'w') as f:
         f.write(html)
