a4e6b0f16eaeac478c4b261a88b283ef7b431099
Ryan Marren <rmarren1@jhu.edu>
2016-11-16 20:39:36 -0500

    made docker setup not use sudo for cortex

--- service/setup_docker.sh
+++ service/setup_docker.sh
@@ -1,2 +1,2 @@
-sudo docker build -t eeg_panda_image .
-sudo docker run --name eeg_panda_container -p 127.0.0.1:8888:80 eeg_panda_image
+docker build -t eeg_panda_image .
+docker run --name eeg_panda_container -p 127.0.0.1:8888:80 eeg_panda_image
