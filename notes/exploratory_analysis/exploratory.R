91eb388039e3056bfe16d9bc5aa27e752b93f071
Nitin Kumar <nkumar14@jhu.edu>
2017-03-12 12:01:25 -0400

    Added joint prob pseudocode

--- notes/exploratory_analysis/exploratory.R
+++ notes/exploratory_analysis/exploratory.R
@@ -11,12 +11,12 @@ eeg_meda <- function(inputpath, outname, start_elec, end_elec, start_time, end_t
         library(h5);
     }
     require(devtools)
-    #devtools::install_github("neurodata/meda")
-    require(meda)
+    devtools::install_github("neurodata/meda")
+    #require(meda)
     require(ggplot2)
     
     file <- h5file(inputpath, "r");
-    outFile <- paste0("C:/Users/Nitin/Documents/Hopkins/BCI/orange-panda/notes/exploratory_analysis/",
+    outFile <- paste0("~/hopkins/orange-panda/notes/exploratory_analysis/",
                       inputpath, "/", outname)
     
     resultdata <- data.matrix(data.frame(file["result/data"][]))
@@ -26,7 +26,7 @@ eeg_meda <- function(inputpath, outname, start_elec, end_elec, start_time, end_t
     if(end_time == 0) {
         end_time = nrow(resultdata)
     }
-    sample_dat <- resultdata[seq(from=start_time,to=end_time,by=100), start_elec:end_elec]
+    sample_dat <- resultdata[seq(from=start_time,to=end_time,by=30), start_elec:end_elec]
     bool <- !apply(sample_dat, 2, function(x) sd(x) == 0)
     sample_dat <- sample_dat[, bool]
     meda::genHTML(sample_dat, outFile)
@@ -40,99 +40,35 @@ run_meda <- function(file_name, start_elec, end_elec, start_time, end_time) {
 }
 
 run_scripts <- function() {
-    # Half partitions of data
-    #run_meda("full_A00051826_01",start_elec=40,end_elec=60,
-    #         start_time=0,end_time=10000)
-    #run_meda("full_A00051826_01",start_elec=40,end_elec=60,
-    #         start_time=10000,end_time=0)
+    patients = c(
+        "gp_A00051826001",
+        "bip_A00053375001",
+        "gip_A00051826005",
+        "gip_A00051955001",
+        "gip_A00053440001",
+        "gp_A00051886001",
+        "oip_A00053398_part2001",
+        "gip_A00054417001",
+        "bip_A00054215001",
+        "gip_A00054207001",
+        "gp_A00054039001",
+        "gp_A00054023001",
+        "gp_A00053990001",
+        "oip_A00053909001",
+        "gp_A00053597001",
+        "oip_A00053480001",
+        "gip_A00053460001"
+    ) 
     
-    # All partitions 60-80
-    #run_meda("full_A00051826_01",start_elec=60,end_elec=80,
-    #         start_time=0,end_time=0)
-    #run_meda("full_A00051826_01",start_elec=60,end_elec=80,
-    #         start_time=0,end_time=10000)
-    #run_meda("full_A00051826_01",start_elec=60,end_elec=80,
-    #         start_time=10000,end_time=0)
-    
-    # All partitions 80-111
-    #run_meda("full_A00051826_01",start_elec=80,end_elec=111,
-    #         start_time=0,end_time=0)
-    #run_meda("full_A00051826_01",start_elec=80,end_elec=111,
-    #         start_time=0,end_time=10000)
-    #run_meda("full_A00051826_01",start_elec=80,end_elec=111,
-    #         start_time=10000,end_time=0)
-    
-    # All partitions 30-70
-    #run_meda("full_A00051826_01",start_elec=30,end_elec=80,
-    #         start_time=0,end_time=0)
-    #run_meda("full_A00051826_01",start_elec=80,end_elec=111,
-    #         start_time=10000,end_time=0)
-    
-
-    
-    #### Test
-    
-    # Half partitions of data
-    #run_meda("test",start_elec=40,end_elec=60,
-    #         start_time=0,end_time=0)
-    #run_meda("test",start_elec=40,end_elec=60,
-    #         start_time=0,end_time=10000)
-    #run_meda("test",start_elec=40,end_elec=60,
-    #         start_time=10000,end_time=0)
-    
-    # All partitions 60-80
-    #run_meda("test",start_elec=60,end_elec=80,
-    #         start_time=0,end_time=0)
-    #run_meda("test",start_elec=60,end_elec=80,
-    #         start_time=0,end_time=10000)
-    #run_meda("test",start_elec=60,end_elec=80,
-    #         start_time=10000,end_time=0)
-    
-    # All partitions 80-111
-    #run_meda("test",start_elec=80,end_elec=111,
-    #         start_time=0,end_time=0)
-    #run_meda("test",start_elec=80,end_elec=111,
-    #         start_time=0,end_time=10000)
-    #run_meda("test",start_elec=80,end_elec=111,
-    #         start_time=10000,end_time=0)
-    
-    # All partitions 30-70
-    #run_meda("test",start_elec=30,end_elec=70,
-    #         start_time=0,end_time=0)
-    #run_meda("test",start_elec=30,end_elec=70,
-    #         start_time=0,end_time=10000)
-    #run_meda("test",start_elec=80,end_elec=111,
-    #         start_time=10000,end_time=0)
-        
+    for(patient in patients) {
+        run_meda(patient,start_elec=0,end_elec=40,
+                 start_time=0,end_time=8000)
+        run_meda(patient,start_elec=40,end_elec=80,
+                 start_time=0,end_time=8000)
+        run_meda(patient,start_elec=80,end_elec=111,
+                 start_time=0,end_time=8000)
+    }
 }
 
-run_scripts2 <- function() {
-    # Paradigm 2
-    #run_meda("full_A00051826_02",start_elec=0,end_elec=40,
-    #         start_time=0,end_time=0)
-    run_meda("full_A00051826_02",start_elec=40,end_elec=80,
-             start_time=0,end_time=0)
-    run_meda("full_A00051826_02",start_elec=80,end_elec=111,
-             start_time=0,end_time=0)
-    ### Paradigm 3
-#    run_meda("full_A00051826_03",start_elec=0,end_elec=40,
-#             start_time=0,end_time=0)
-#    run_meda("full_A00051826_03",start_elec=40,end_elec=80,
-#             start_time=0,end_time=0)
-#    run_meda("full_A00051826_03",start_elec=80,end_elec=111,
-#             start_time=0,end_time=0)
-    ### Paradigm 4
-#    run_meda("full_A00051826_04",start_elec=0,end_elec=40,
-#             start_time=0,end_time=0)
-#    run_meda("full_A00051826_04",start_elec=40,end_elec=80,
-#             start_time=0,end_time=0)
-#    run_meda("full_A00051826_04",start_elec=80,end_elec=111,
-#             start_time=0,end_time=0)
-#    ### Paradigm 5
-#    run_meda("full_A00051826_02",start_elec=0,end_elec=40,
-#             start_time=0,end_time=0)
-#    run_meda("full_A00051826_02",start_elec=40,end_elec=80,
-#             start_time=0,end_time=0)
-#    run_meda("full_A00051826_02",start_elec=80,end_elec=111,
-#             start_time=0,end_time=0)    
-}
+run_scripts()
+traceback()
